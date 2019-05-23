c0a29a82503a31657920c7a59540e7a1b384016f
giapbnv <nguyen.van.giapb@sun-asterisk.com>
2019-05-23 11:42:56 +0700

    Chapter10: update users, requires login to edit...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_to current_user
+      redirect_back_or user
     else
       flash[:danger] = I18n.t "sessions.alert.danger"
       render :new
